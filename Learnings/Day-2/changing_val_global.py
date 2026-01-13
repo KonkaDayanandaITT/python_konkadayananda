434beac1dfe5ce3f168637070288b38c8674e2f9
KonkaDayananda <dayakonka@gmail.com>
2026-01-13 17:23:50 +0530

    Completed Day-2 tasks and things

---
+++ Learnings/Day-2/changing_val_global.py
@@ -0,0 +1,14 @@
+x = 100
+
+def change():
+    global x
+    x = 20
+
+
+change()
+print(x)
+
+
+
+
+
---
+++ Learnings/Day-2/if_elif.py
@@ -0,0 +1,17 @@
+marks = 100
+
+if marks >=81 & marks <= 100:
+    print("A+ grade")
+
+elif marks >= 80:
+    print("A grade")
+
+elif marks >= 60:
+    print("B grade")
+
+elif marks >= 45:
+    print("C grade")
+
+else:
+    print("Not passed")
+    
