3f3572f7075fbe28b1fd0ebe261c5ee8fd20f025
KonkaDayananda <dayakonka@gmail.com>
2026-02-25 17:33:02 +0530

    Completed Day-3 topics

---
+++ Week_7/Learnings/Day-3/oop.py
@@ -0,0 +1,66 @@
+# class Person:
+#   def __init__(self , name, age):
+#     self.name = name
+#     self.age = age
+
+#   def greet(self):
+#     print("Hello, my name is " + self.name)
+
+# p1 = Person("Ram", 25)
+# p1.greet()
+
+# class Person:
+#   def __init__(this, name, age):
+#     this.name = name
+#     this.age = age
+
+#   def greet(this):
+#     print("Hello, my name is " + this.name)
+
+# p1 = Person("Ram", 25)
+# p1.greet()
+
+# class A:
+#     def __init__(self):
+#         print("A")
+
+# class B(A):
+#     def __init__(self):
+#         super().__init__()
+#         print("B")
+
+# B()
+
+# class A:
+#     def __init__(self):
+#         print("A")
+# class B(A):        
+#     def __init__(self):
+#         #super().__init__()
+#         print("B")
+        
+# #A()
+# # B()
+# a = B() 
+# a
+
+# class A:
+#     def __init__(self):
+#         print("A")
+
+# class B(A):
+#     def __init__(self):
+#         print("B")
+#         super().__init__()
+
+# class C(A):
+#     def __init__(self):
+#         print("C")
+#         super().__init__()
+
+# class D(B, C):
+#     def __init__(self):
+#         print("D")
+#         super().__init__()
+        
+# print(D())
